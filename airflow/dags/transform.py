97b0ab79f59fb520003abf158dd9e44b7ebde72f
Jennifer Wu <71246844+Jenniferwyqq@users.noreply.github.com>
2022-12-06 22:13:08 -0800

    Update transform.py

--- airflow/dags/transform.py
+++ airflow/dags/transform.py
@@ -9,17 +9,34 @@ def transform():
   
   # transform value
   train_df['Category'] = train_df['Category'].str.upper()  
-  train_df['Color'] = train_df['Color'].str.upper() 
+  train_df['Color'] = train_df['Color'].str.upper()
   train_df['Material'] = train_df['Material'].str.upper()
   
   # transform color
-  # train_df['Color'] = train_df['Color'].replace(['30days','35days'],'40days')
+  train_df['Color'] = train_df['Color'].replace(['LIGHT-BLUE', 'DARK-BLUE', 'NAVY'],'BLUE')
+  train_df['Color'] = train_df['Color'].replace(['BACK','CROCO-BLACK'],'BLACK')
+  train_df['Color'] = train_df['Color'].replace(['D-BROWN', 'DARK-TAN', 'DK-NAT'],'DARK-BROWN')
+  train_df['Color'] = train_df['Color'].replace(['DURSTY', 'L-GREY', 'D-GREY', 'GREY'],'GRAY')
+  train_df['Color'] = train_df['Color'].replace(['OLIVE', 'GRASS'],'GREEN')
+  train_df['Color'] = train_df['Color'].replace(['OAT-CHEET', 'KHAK'],'KHAKI-CAMO')
+  train_df['Color'] = train_df['Color'].replace(['L-BROWN', 'NAT', 'TAN'],'LIGHT-BROWN')
+  train_df['Color'] = train_df['Color'].replace(['LIGHT PINK', 'L-PINK', 'N-PINK'],'NEON-PINK')
+  train_df['Color'] = train_df['Color'].replace(['LIGHT YELLOW', 'L-YELLOW', 'N-YELLOW'],'NRON-YELLOW')
+  train_df['Color'] = train_df['Color'].replace(['WINE', 'LIPS', 'DARK-RED'],'RED')
+  train_df['Color'] = train_df['Color'].replace(['O-RG', 'ORANGE', 'YEL'],'YELLOW')
+  
   
   # transform category
-  # train_df['Category'] = train_df['Category'].replace(['30days','35days'],'40days')
+  train_df['Category'] = train_df['Category'].replace(['S-BOOT', 'BOOTS'],'BOOT')
+  train_df['Category'] = train_df['Category'].replace(['HEEL PUMP', 'HEELS'],'HEEL_PUNP')
+  train_df['Category'] = train_df['Category'].replace(['LOWER SANDAL', 'HEEL SAMDAL', 'SAND'],'SANDAL')
+  train_df['Category'] = train_df['Category'].replace(['SHOE', 'SNEAK', 'FLAT', 'FLAT SNEAKER'],'SNEAKER')
   
   # transform material
-  # train_df['Material'] = train_df['Material'].replace(['30days','35days'],'40days')
+  train_df['Material'] = train_df['Material'].replace(['LEA', 'LEATH'],'LEATHER')
+  train_df['Material'] = train_df['Material'].replace(['NB', 'NPU'],'NBPU')
+  train_df['Material'] = train_df['Material'].replace(['ISU', 'SU', 'SUEDE'],'IMSU')
+  train_df['Material'] = train_df['Material'].replace(['BRIGHT-PAT', 'P'],'PAT')
   
   
   # groupby the df
